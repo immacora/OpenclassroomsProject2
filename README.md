9b481e91d90a6d325c8ab614f7250199691ece87
Coralie <immacora@gmail.com>
2022-07-08 12:35:26 +0200

    Déplacement de la création des jpg dans la fonction book_page_data

--- README.md
+++ README.md
@@ -16,13 +16,13 @@ Prérequis :
 Pour utiliser le programme :
 
 Assurez-vous d'avoir installé la version 3.10.5 de Python
-Téléchargez les fichiers du projet dans l'IDE PyCharm : 
+
+Téléchargez les fichiers du projet dans l'IDE PyCharm via VCS et vérifiez les éléments suivants lors du clonage : 
   - Environment Virtualenv
   - Base interpreter Python310
+  - Fichier requirements
 
-Installez les packages depuis le fichier requirements
+Vérifier l'import et lancez le programme depuis le main
 
-Lancez le programme
-
-L'exécution du programme dure environ 25 minutes.
+L'exécution du programme dure environ 15 minutes.
 Une fois terminé, un fichier csv sera créé pour chaque catégorie de livre dans le dossier data du projet. Le répertoire data contient le répertoire img des fichiers images téléchargés des livres.
--- function.py
+++ function.py
@@ -1,9 +1,10 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import re
 
 
-##Retourne la liste des catégories du site Web Books to scrape##
+# Retourne la liste des catégories du site Web Books to scrape
 def category_list(url):
     response = requests.get(url)
     category_url = []
@@ -16,22 +17,21 @@ def category_list(url):
                 link = a.get('href')
                 link = link.replace('..', 'http://books.toscrape.com/catalogue/category')
                 category_url.append(link)
+
         return category_url
+
     else:
         return response
 
 
-##Récupérer les href de tous les livres d'une catégorie##
+# Récupérer les href de tous les livres d'une catégorie
 def books_category(url):
     response = requests.get(url)
     url_books_category = []
-
     # Si l'url est valide
     if response.ok:
         soup = BeautifulSoup(response.content, 'lxml')
         next_btn = soup.find('li', class_='next')
-        category = soup.find('li', class_='active').text
-
         # Si le bouton next n'existe pas
         if next_btn is None:
             links = soup.find('ol').findAll('a')
@@ -42,16 +42,16 @@ def books_category(url):
                 # trier les doublons
                 if link not in url_books_category:
                     url_books_category.append(link)
+
             return url_books_category
 
         # Si le bouton next existe
         else:
             nb_pages = soup.find('li', class_='current').text
             nb_pages = int(re.search('of (.+?)', nb_pages).group(1))
-
             # Créer la liste des url des pages de la catégorie
-            for i in range(nb_pages):
-                url_page_categorie = url.replace('index', "page-" + str(i + 1))
+            for k in range(nb_pages):
+                url_page_categorie = url.replace('index', "page-" + str(k + 1))
                 response = requests.get(url_page_categorie)
                 # Si l'url est valide
                 if response.ok:
@@ -71,28 +71,24 @@ def books_category(url):
         return response
 
 
-##Retourne les données formatées de la page dans une liste de listes##
+# Retourne les données formatées de la page dans un dictionnaire
 def book_page_data(url):
     response = requests.get(url)
-
     # Si l'url est valide
     if response.ok:
         soup = BeautifulSoup(response.content, 'lxml')
-        dico_page_data = {'': []}
-
+        # dico_page_data = {'': []}
         product_page_url = url
         titre = soup.find('h1').text
         product_description = soup.select_one('article > p').text.replace(' ...more', '')
         category = soup.select_one('li:nth-of-type(3)').find('a').text
         review_rating = soup.find('p', class_='star-rating').get('class').pop()
         image_url = soup.find('img').get('src').replace('../..', 'http://books.toscrape.com')
-
         table = soup.find_all('td')
         universal_product_code = table[0].text
         price_including_tax = table[2].text
         price_excluding_tax = table[3].text
         number_available = table[5].text.removeprefix('In stock (').removesuffix(' available)')
-
         dico_page_data = {'titre': titre,
                           'product_page_url': product_page_url,
                           'universal_product_code': universal_product_code,
@@ -103,6 +99,10 @@ def book_page_data(url):
                           'category': category,
                           'review_rating': review_rating,
                           'image_url': image_url}
+        # Télécharge les jpg des livres
+        img_data = requests.get(url).content
+        with open(os.path.join('data/img/') + universal_product_code + '.jpg', 'wb') as handler:
+            handler.write(img_data)
 
         return dico_page_data
 
